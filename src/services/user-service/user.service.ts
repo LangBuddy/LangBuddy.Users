650435b0d2a5fcfd50b77fb11ab053e90620ff8f
fremyxs <antonovch13@mail.ru>
2023-09-17 11:41:02 +0500

    feat(services): change service UserService
    Add a condition to the selection so that deleteDate is null
    Add update deleteDate and updateDate when delete or update actions occur

--- src/services/user-service/user.service.ts
+++ src/services/user-service/user.service.ts
@@ -1,9 +1,10 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../../database/entities/user.entity';
-import { Repository } from 'typeorm';
+import { IsNull, Repository } from 'typeorm';
 import { UserCreateRequest } from '../../models/request/user.create.request';
 import { UserUpdateRequest } from '../../models/request/user.update.request';
+import { UserResponse } from 'src/models/response/user.response';
 
 @Injectable()
 export class UserService {
@@ -14,12 +15,16 @@ export class UserService {
 
   async create(userCreateRequest: UserCreateRequest) {
     const user = this.userRepository.create(userCreateRequest);
+    user.setCreateDate();
     return await this.userRepository.save(user);
   }
 
   async findOne(id: number) {
-    const user = await this.userRepository.findOneBy({
-      id: id,
+    const user = await this.userRepository.findOne({
+      where: {
+        id,
+        deleteDate: IsNull(),
+      },
     });
 
     if (!user) {
@@ -30,18 +35,35 @@ export class UserService {
   }
 
   async findAll() {
-    const users = await this.userRepository.find();
+    const users = (
+      await this.userRepository.find({
+        where: {
+          deleteDate: IsNull(),
+        },
+      })
+    ).map((el) => {
+      const userResponse: UserResponse = {
+        firstName: el.firstName,
+        lastName: el.lastName,
+        birthday: el.birthday,
+        gender: el.gender,
+      };
+      return userResponse;
+    });
+
     return users;
   }
 
   async update(id: number, userUpdateRequest: UserUpdateRequest) {
     const user = await this.findOne(id);
+    user.setUpdateDate();
 
     await this.userRepository.update(id, {
       firstName: userUpdateRequest.firstName || user.firstName,
       lastName: userUpdateRequest.lastName || user.lastName,
       birthday: userUpdateRequest.birthday || user.birthday,
       gender: userUpdateRequest.gender || user.gender,
+      updateDate: user.updateDate,
     });
 
     return `This action updates a #${id} user`;
@@ -49,8 +71,9 @@ export class UserService {
 
   async delete(id: number) {
     const user = await this.findOne(id);
+    user.setDeleteDate();
 
-    await this.userRepository.remove(user);
+    await this.userRepository.update(id, user);
 
     return `This action removes a #${id} user`;
   }
